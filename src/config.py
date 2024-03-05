35e0cacf45f030b2a146571a19e7af8b3412a9ea
Belal Elsabbagh <belsabbagh@gmail.com>
2024-03-05 11:54:42 +0200

    Update server_dump.py to use pickle instead of json for dumping server data

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import datetime
 
 INCREMENT_MINUTES = 1
 REFRESH_RATE = 0
-NUM_HOUSES = 100
+NUM_HOUSES = 16
 SERVER_ADDRESS = ("localhost", 9405)
 START_DATE = datetime.datetime(2010, 1, 1, 7, 0, 0)
 END_DATE = datetime.datetime(2010, 1, 1, 19, 0, 0)
