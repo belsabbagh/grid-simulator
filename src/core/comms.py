4171fcc6b4f184cf11026d1a56362c730a3671ea
Belal Elsabbagh <belsabbagh@gmail.com>
2024-03-06 19:16:39 +0200

    wrote new meter rethreading

---
+++ src/core/comms.py
@@ -0,0 +1,152 @@
+import pickle
+import select
+import socket
+import threading
+import time
+from typing import Iterable
+
+from pyparsing import Any
+
+
+def connect_sockets(sockets, addr):
+    try:
+        for s in sockets:
+            s.connect(addr)
+    except ConnectionRefusedError as e:
+        raise ConnectionRefusedError(
+            f"Socket {s.getsockname()} failed to connect to {addr}."
+        ) from e
+
+
+def connect_to(addr: tuple[str, int]) -> socket.socket:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(addr)
+    return s
+
+
+def close_connection(s: socket.socket):
+    s.close()
+
+
+def send_data(s: socket.socket, data: Any):
+    s.sendall(pickle.dumps(data))
+
+
+def recv_data(s: socket.socket, buf_size: int, timeout: int) -> Any:
+    s.settimeout(timeout)
+    recv_stream: bytes = s.recv(buf_size)
+    return pickle.loads(recv_stream)
+
+
+def listen_for_duration(
+    s: socket.socket, buf_size: int, duration: int
+) -> dict[socket.socket, bytes]:
+    messages = {}
+    start_time = time.time()
+    end_time = start_time + duration
+    while time.time() < end_time:
+        conn, _ = s.accept()
+        messages[conn] = conn.recv(buf_size)
+    return messages
+
+
+def concurrent_recv(sockets: Iterable[socket.socket], buf_size: int, timeout: int):
+    """
+    Takes a list of sockets and listens to them in parallel.
+    It stops on the first message received from any of the sockets.
+    """
+    received_data = {s: b"" for s in sockets}
+    ready_to_read = list(sockets)
+
+    while True:
+        readable, _, _ = select.select(ready_to_read, [], [], timeout)
+
+        if readable:
+            for s in readable:
+                data = s.recv(buf_size)
+                received_data[s] += data
+                if data:
+                    return s, received_data[s]
+
+        ready_to_read = [s for s in ready_to_read if s not in readable]
+        if timeout == 0:
+            return None, None
+
+
+def concurrent_recv_duration(
+    sockets: Iterable[socket.socket], buf_size: int, duration: int
+):
+    """
+    Takes a list of sockets and listens to them in parallel for a specified duration.
+    It reads from all sockets during the specified duration and returns the received data.
+    """
+    received_data = {s: b"" for s in sockets}
+    ready_to_read = list(sockets)
+
+    start_time = time.time()
+    end_time = start_time + duration
+
+    while time.time() < end_time:
+        readable, _, _ = select.select(ready_to_read, [], [], end_time - time.time())
+
+        if readable:
+            for s in readable:
+                data = s.recv(buf_size)
+                received_data[s] += data
+
+        ready_to_read = [s for s in ready_to_read if s not in readable]
+
+    return received_data
+
+
+def make_sockets_handler(init=None):
+    sockets: dict[tuple[str, int], socket.socket] = init or {}
+
+    def _connect_to(addr: tuple[str, int]) -> None:
+        s = connect_to(addr)
+        sockets[addr] = s
+
+    def _close_connection(addr: tuple[str, int]) -> None:
+        s = sockets[addr]
+        close_connection(s)
+        del sockets[addr]
+
+    def _send_data(addr: tuple[str, int], data: Any) -> None:
+        s = sockets[addr]
+        send_data(s, data)
+
+    def _recv_data(addr: tuple[str, int], buf_size: int, timeout: int) -> Any:
+        s = sockets[addr]
+        return recv_data(s, buf_size, timeout)
+
+    def sockets_iterator():
+        yield from sockets.items()
+
+    def _concurrent_recv(buf_size: int, timeout: int) -> tuple[tuple[str, int], Any]:
+        s, data = concurrent_recv(
+            map(lambda x: x[1], sockets_iterator()), buf_size, timeout
+        )
+        addr = next(addr for addr, sock in sockets.items() if sock == s)
+        return addr, data
+
+    def _concurrent_recv_duration(
+        buf_size: int, duration: int
+    ) -> dict[tuple[str, int], Any]:
+        received_data = concurrent_recv_duration(
+            map(lambda x: x[1], sockets_iterator()), buf_size, duration
+        )
+        return {s.getpeername(): data for s, data in received_data.items()}
+
+    return (
+        _connect_to,
+        _close_connection,
+        _send_data,
+        _recv_data,
+        sockets_iterator,
+        _concurrent_recv,
+        _concurrent_recv_duration,
+    )
+
+
+def make_msg_body(addr, _type, **kwargs):
+    return {"from": addr, "type": _type, **kwargs}
--- src/server/sim.py
+++ src/server/sim.py
@@ -1,12 +1,18 @@
+import pickle
 import socket
 import threading
 import time
 import socket
+from src.config import DEVIATION
+from src.core.comms import connect_sockets, make_msg_body
+from src.core.data_generator import mk_grid_state_generator, mk_instance_generator
+from src.core.meter import mk_meter, mk_meters_handler
+from src.core.optimizer import mk_choose_best_offers_function
 
 from src.core.util import date_range
-from src.core.meter import mk_meters_runner
+from src.presets import mk_meters_runner
 
-def make_simulation_server_state(n, server_address, append_state):
+def make_simulation_server(n, server_address, append_state):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(server_address)
         s.listen(n)
@@ -15,6 +21,7 @@ def make_simulation_server_state(n, server_address, append_state):
         print("Waiting to connect to meters...")
         meters_runner = mk_meters_runner(n, server_address)
         meters_thread = threading.Thread(target=meters_runner)
+        meters_thread.daemon = True
         meters_thread.start()
         for _ in range(n):
             conn, addr = s.accept()
@@ -26,5 +33,47 @@ def make_simulation_server_state(n, server_address, append_state):
             print(f"Moment {t} has passed.")
             append_state(state)
             time.sleep(refresh_rate)
+
+    return simulate
+
+
+def make_persistent_simulation_server(n, server_address, append_state):
+    trade_chooser = mk_choose_best_offers_function(
+        "models/grid-loss.h5",
+        "models/duration.h5",
+        "models/grid-loss.h5",
+    )
+    sockets = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for _ in range(n)]
+    connect_sockets(sockets, server_address)
+    sockets_dict = {
+        s.getsockname(): mk_meter(s, None, None, None, trade_chooser) for s in sockets
+    }
+    start_threads, join_threads, fetch_state = mk_meters_handler(sockets_dict)
+
+    def periodic_fetch_state():
+        while True:
+            append_state(fetch_state())
+
+    fetch_state_thread = threading.Thread(target=periodic_fetch_state)
+
+    data_generator = mk_instance_generator(DEVIATION)
+    grid_state_generator = mk_grid_state_generator()
+
+    def simulate(start_date, end_date, datetime_delta, refresh_rate):
+        fetch_state_thread.start()
+        for t in date_range(start_date, end_date, datetime_delta):
+            grid_state = grid_state_generator(t)
+            for s in sockets:
+                gen, con = data_generator(t)
+                msg = make_msg_body(
+                    s.getsockname(),
+                    "power",
+                    datetime=t,
+                    grid_state=grid_state,
+                    generation=gen,
+                    consumption=con,
+                )
+                s.sendall(pickle.dumps(msg))
+                time.sleep(refresh_rate)
 
     return simulate
