f1678d8274bcbc16d6b0af0b8a8540e6e938e9dd
Belal Elsabbagh <belsabbagh@gmail.com>
2024-03-11 08:20:50 +0200

    Add run retrieval functions and update Flask server configuration

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+from typing import Callable, Optional
 from flask import Flask, jsonify
 from flask_cors import CORS
 import datetime
@@ -17,11 +18,46 @@ from src.config import (
     REFRESH_RATE,
 )
 
+def get_run(runs_folder: str, run_id: str) -> Optional[dict]:
+    """Get a run from the specified folder.
+
+    Args:
+        run_id (str): The name of the run.
+        runs_folder (str): The folder where the runs are stored.
+    Returns:
+        Optional[dict]: The run if it exists, otherwise None.
+    """
+    try:
+        with open(os.path.join(runs_folder, run_id + ".pkl"), "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return None
+
+
+def get_run_meta(runs_folder: str, run_id: str) -> Optional[dict]:
+    """Get the time created of a run."""
+    try:
+        ctime = os.path.getctime(os.path.join(runs_folder, run_id + ".pkl"))
+        run = get_run(runs_folder, run_id)
+        if run is None:
+            return None
+        parameters = run["parameters"]
+        del run
+        return {"id": run_id, "created": datetime.datetime.fromtimestamp(ctime), "parameters": parameters}
+    except FileNotFoundError:
+        return None
+
 
 def create_flask_server(
-    runs_folder,
-    cors_endpoints=None,
-):
+    runs_folder: str,
+) -> Callable[[], None]:
+    """Create a Flask server that serves the next state of a real-time simulation and the recorded runs in the specified folder.
+
+    Args:
+        runs_folder (str): The folder where the recorded runs are stored.
+    Returns:
+        Callable[[], None]: A function that starts the server.
+    """
     append_state, fetch_next_state, _, _ = make_buffer()
 
     def make_simulate_thread():
@@ -51,12 +87,7 @@ def create_flask_server(
         return simulate_thread
 
     app = Flask(__name__)
-    if cors_endpoints is None:
-        cors_endpoints = [
-            r"/*/*",
-        ]
-
-    cors_resources = {endpoint: {"origins": "*"} for endpoint in cors_endpoints}
+    cors_resources = {r"/*/*": {"origins": "*"}}
     cors = CORS(app, resources=cors_resources)
 
     simulate_thread = make_simulate_thread()
@@ -70,14 +101,20 @@ def create_flask_server(
     @app.route("/runs", methods=["GET"])
     def playback_runs():
         return jsonify(
-            {"runs": [os.path.splitext(f)[0] for f in os.listdir(runs_folder)]}
+            {
+                "runs": [
+                    get_run_meta(runs_folder, os.path.splitext(f)[0])
+                    for f in os.listdir(runs_folder)
+                ]
+            }
         )
 
     @app.route("/runs/<string:run_id>", methods=["GET"])
     def playback_parameters(run_id):
         nonlocal record
-        with open(os.path.join(runs_folder, run_id + ".pkl"), "rb") as f:
-            record = pickle.load(f)
+        record = get_run(runs_folder, run_id)
+        if record is None:
+            raise FileNotFoundError(f"Run {run_id} not found.")
         return jsonify({"parameters": record["parameters"]})
 
     @app.route("/runs/<string:run_id>/states/<int:idx>", methods=["GET"])
